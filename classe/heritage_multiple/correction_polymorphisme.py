55f881cd9541bbe5058faa84e8d3a678f751c27a
PaulErnest <PaulErMartin@yahoo.fr>
2025-04-04 09:53:42 +0200

    correction polymorphisme

--- classe/heritage_multiple/correction_polymorphisme.py
+++ classe/heritage_multiple/correction_polymorphisme.py
@@ -4,24 +4,32 @@ class Instrument(ABC):
     @abstractmethod
     def jouer(self):
         pass
+    
+    @abstractmethod
+    def accorder(self):
+        pass
 
 class Guitare(Instrument):
     def jouer(self):
         print("Dring")
+    def accorder(self):
+        pass
 
 class Piano(Instrument):
     def jouer(self):
         print("Piaout")
+    def accorder(self):
+        pass
     
 class Batterie(Instrument):
     def jouer(self):
         print("Boom")
+    def accorder(self):
+        pass
 
 def faire_jouer(liste_instrument : list[Instrument]) -> None :
     for instrument in liste_instrument:
         instrument.jouer()
-
-
 
 liste_instrument = [Guitare(), Piano(), Batterie()]
 faire_jouer(liste_instrument)
